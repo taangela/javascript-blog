ef4a2ff2b23e15df5d60eae5975cfac5b8171dfa
Marta Koperek <makoperek@gmail.com>
2019-10-09 10:22:34 +0200

    Add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -5,33 +5,37 @@
   console.log('links:', links);
 });*/
 
-const titleClickHandler = function(event){
-  console.log('Link was clicked!');
-  console.log (event);
+const titleClickHandler = function(event) {
+    event.preventDefault();
+    const clickedElement = this;
+    console.log('Link was clicked!');
+    console.log(event);
 
-  /* remove class 'active' from all article links  */
-  const activeLinks = document.querySelectorAll('.titles a.active');
 
-  for(let activeLink of activeLinks){
-  	activeLink.classList.remove('active');
-  }
-  /* add class 'active' to the clicked link */
-	
-  /* remove class 'active' from all articles */
-	const activeArticles = document.querySelectorAll('.posts article.active');
+    /* [DONE] remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-  for(let activeArticle of activeArticles){
-  	activeArticle.classList.remove('active');
-  }
-  /* get 'href' attribute from the clicked link */
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
+    /* [DONE] add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement)
+    clickedElement.classList.add('active');
+    /*[DONE] remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts article.active');
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
+    /* get 'href' attribute from the clicked link */
 
-  /* add class 'active' to the correct article */
+    /* find the correct article using the selector (value of 'href' attribute) */
+
+    /* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
 }
